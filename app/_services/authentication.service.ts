563cd68a31eeab678cc0c9b851ad3174389a1d27
Jason Watmore <jason@pointblankdevelopment.com.au>
2017-06-17 16:05:34 +1000

    tweaked auth service
    added return user from login method so it gets passed through to login
    component

--- app/_services/authentication.service.ts
+++ app/_services/authentication.service.ts
@@ -16,6 +16,8 @@ export class AuthenticationService {
                     // store user details and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify(user));
                 }
+
+                return user;
             });
     }
 
