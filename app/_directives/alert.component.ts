62462c86c16dff39839c66002db394b280975cf9
Jason Watmore <cornflourblue@hotmail.com>
2017-06-26 14:21:58 +1000

    Updated alert component

--- app/_directives/alert.component.ts
+++ app/_directives/alert.component.ts
@@ -1,4 +1,5 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 import { AlertService } from '../_services/index';
 
@@ -8,12 +9,17 @@ import { AlertService } from '../_services/index';
     templateUrl: 'alert.component.html'
 })
 
-export class AlertComponent {
+export class AlertComponent implements OnDestroy {
+    private subscription: Subscription;
     message: any;
 
-    constructor(private alertService: AlertService) { }
+    constructor(private alertService: AlertService) { 
+        // subscribe to alert messages
+        this.subscription = alertService.getMessage().subscribe(message => { this.message = message; });
+    }
 
-    ngOnInit() {
-        this.alertService.getMessage().subscribe(message => { this.message = message; });
+    ngOnDestroy(): void {
+        // unsubscribe on destroy to prevent memory leaks
+        this.subscription.unsubscribe();
     }
 }
