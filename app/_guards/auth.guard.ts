9bfb7df54a7f278860eeabdb18070e7767226b4b
Jason Watmore <jason.watmore@tabcorp.com.au>
2018-01-24 09:30:54 +1100

    removing leading slash from router redirect

--- app/_guards/auth.guard.ts
+++ app/_guards/auth.guard.ts
@@ -13,7 +13,7 @@ export class AuthGuard implements CanActivate {
         }
 
         // not logged in so redirect to login page with the return url
-        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+        this.router.navigate(['login'], { queryParams: { returnUrl: state.url }});
         return false;
     }
 }
--- app/register/register.component.ts
+++ app/register/register.component.ts
@@ -23,7 +23,7 @@ export class RegisterComponent {
             .subscribe(
                 data => {
                     this.alertService.success('Registration successful', true);
-                    this.router.navigate(['/login']);
+                    this.router.navigate(['login']);
                 },
                 error => {
                     this.alertService.error(error);
